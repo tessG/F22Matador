476abda8f1f0a8d9be77294b83336141a2821337
Tess Gaston Løvgreen <tess@lovegreen.life>
2022-03-14 11:23:33 +0100

    game instanstiating Board

--- src/FileIO.java
+++ src/FileIO.java
@@ -6,24 +6,37 @@ import java.util.Scanner;
 public class FileIO {
 
 
-    ArrayList<String> readGameData(){
-
-        File file = new File("src/data.txt");
+    ArrayList<String> readGameData() {
+        File file = new File("src/data_.txt");
         ArrayList<String> data = new ArrayList<>();
-
         try {
             Scanner scan = new Scanner(file);
-            while(scan.hasNextLine()){
+            while (scan.hasNextLine()) {
                 data.add(scan.nextLine());//“Egon: 30000”
             }
-        }catch(FileNotFoundException e){
-
-
+        } catch (FileNotFoundException e) {
             data = null;
-
-
-
         }
         return data;
     }
+
+
+    //Gruppens arbejde:
+  /*  ArrayList<String> readFieldData() {
+
+        File file = new File("Field.txt");
+        ArrayList<String> field = new ArrayList<>();
+
+        try {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                field.add(scan.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+
+            field = null;
+
+        }
+        return field;
+}*/
 }
