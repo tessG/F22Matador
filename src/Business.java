b40629f712c1206c774623f2460160d63a0da518
Tess Gaston Løvgreen <tess@lovegreen.life>
2022-03-29 09:48:00 +0200

    formatting

--- src/Business.java
+++ src/Business.java
@@ -2,5 +2,4 @@ public class Business extends Property {
     public Business( int id, String label, int cost, int income, int seriesID ) {
         super(id, label, cost, income, seriesID);
     }
-
 }
